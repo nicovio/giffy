9b4236e762d8f1de46c3686c8cbebc6d562006e5
nicovio <nicolasviotti90@gmail.com>
2021-11-06 03:35:05 -0300

    Termino búsqueda

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import { Link } from 'wouter'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'wouter'
+import ListOfGifs from '../components/ListOfGifs'
+import useGifs from '../hooks/useGifs'
 import '../styles/Home.css'
 
 const GIFS_POPULARES = ['Matrix', 'Argentina', 'Colombia', 'Ecuador']
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [path, pushLocation] = useLocation()
+  const { loading, gifs } = useGifs()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    pushLocation(`/search/${keyword}`)
+  }
+
+  const handleChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
   return (
-    <div className="links-container">
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input aria-label="Buscar" placeholder="Buscar..." type="text" onChange={handleChange} value={keyword} />
+        <button>Buscar</button>
+      </form>
+      <h3>Última busqueda</h3>
+      <ListOfGifs gifs={gifs} loading={loading} />
       <h3>Los gifs más populares</h3>
       <ul>
         {GIFS_POPULARES.map((gif) => (

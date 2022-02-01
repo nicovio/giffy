ea94fd4129172dee40ad38c5f4d777205c4f6981
nicovio <nicolasviotti90@gmail.com>
2022-02-01 20:22:42 -0300

    Saco spinner de componente Category

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -1,25 +1,19 @@
-import Spinner from 'components/Spinner/Spinner'
 import React from 'react'
 import './Category.css'
 import CategoryListItem from './CategoryListItem'
 
 export default function Category({ name, options = [], loading }) {
-  const spinnerStyle = { marginTop: '2rem' }
 
   return (
     <>
       <h2 className="App-title Category-title">{name}</h2>
-      {loading ? (
-        <Spinner style={spinnerStyle} />
-      ) : (
-        <>
-          <ul className="Category-list">
-            {options.map((option, index) => (
-              <CategoryListItem key={option} option={option} index={index} />
-            ))}
-          </ul>
-        </>
-      )}
+      <>
+        <ul className="Category-list">
+          {options.map((option, index) => (
+            <CategoryListItem key={option} option={option} index={index} />
+          ))}
+        </ul>
+      </>
     </>
   )
 }
--- frontend/src/components/TrendingSearches/TrendingSearches.jsx
+++ frontend/src/components/TrendingSearches/TrendingSearches.jsx
@@ -4,21 +4,18 @@ import { gifService } from 'services/gifService'
 
 export default function TrendingSearches() {
   const [trends, setTrends] = useState([])
-  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchTrends = async () => {
-      setLoading(true)
       const trends = await gifService.getTrendingTerms()
       setTrends(trends)
-      setLoading(false)
     }
     fetchTrends()
   }, [])
 
   return (
     <>
-      <Category name="Tendencias" options={trends} loading={loading}></Category>
+      <Category name="Tendencias" options={trends} ></Category>
     </>
   )
 }
